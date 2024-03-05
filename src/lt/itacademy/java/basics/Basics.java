32fd1af88d31cc7e1d39ed5af0626324dea5aad3
Jamtit <majauskas.titas@gmail.com>
2024-03-05 14:49:13 +0200

    Task 2

--- src/lt/itacademy/java/basics/Basics.java
+++ src/lt/itacademy/java/basics/Basics.java
@@ -5,15 +5,26 @@ import java.util.Scanner;
 public class Basics {
 
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
-        System.out.println("What is your surname?");
-        String surname = scanner.nextLine();
-        System.out.println("How old are you?");
-        int age = scanner.nextInt();
-        scanner.close();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("What is your name?");
+//        String name = scanner.nextLine();
+//        System.out.println("What is your surname?");
+//        String surname = scanner.nextLine();
+//        System.out.println("How old are you?");
+//        int age = scanner.nextInt();
+//        scanner.close();
+//
+//        System.out.println("Hello my name is " + name + " " + surname + " I am " + age + " years old. I am attending IT & Data Academy 2024!");
+        boolean primBoolean = true;
+        char myCharH = 'H';
+        char myCharW = 'w';
+        char myCharR = 'r';
+        char myCharD = 'd';
+        byte myByte = 3;
+        int myInt = 1;
+        float myFloat = 2.0f;
+        short myShort = 0;
 
-        System.out.println("Hello my name is " + name + " " + surname + " I am " + age + " years old. I am attending IT & Data Academy 2024!");
+        System.out.println("" + myCharH + myByte + myInt + myInt + myShort + " " + myCharW + myShort + myCharR + myInt + myCharD + " " + myFloat + " " + primBoolean);
     }
 }
