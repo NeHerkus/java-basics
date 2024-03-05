75e26d1514a3a58c3d57f67fef4628c96443365e
simzub <78310955+simzub@users.noreply.github.com>
2024-03-05 14:32:27 +0200

    add task 2

--- src/lt/itacademy/java/basics/Basics.java
+++ src/lt/itacademy/java/basics/Basics.java
@@ -3,8 +3,9 @@ package lt.itacademy.java.basics;
 import java.util.Scanner;
 
 public class Basics {
-    // Shortcut: main
+
     public static void main(String[] args) {
+        /*
         Scanner myObj = new Scanner(System.in);
 
         System.out.println("Enter your name:");
@@ -18,6 +19,16 @@ public class Basics {
 
         System.out.println("Hello my name is " + name + " " + surname + " and I am " + age + " years old. I am attending IT & Data\n" +
                 "Academy 2024!");
-    }
 
+         */
+
+        byte byteVal = 0;
+        short shortVal = 1;
+        int intVal = 3110;
+        float floatVal = 2.0f;
+        boolean booleanVal = true;
+        char charVal = 'd';
+
+        System.out.println("H" + intVal + " w" + byteVal + "r" + shortVal + charVal + " " + floatVal + " " + booleanVal);
+    }
 }
