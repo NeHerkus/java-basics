ec93ca4c58056ce639df24ac6de1d523a03731fb
Ingrida <ingridarrr@gmail.com>
2024-03-05 14:38:48 +0200

    second task

--- src/lt/itacademy/java/basics/Basics.java
+++ src/lt/itacademy/java/basics/Basics.java
@@ -6,19 +6,42 @@ public class Basics {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input your name: ");
-        String name = scanner.nextLine();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Input your name: ");
+//        String name = scanner.nextLine();
+//
+//        System.out.println("Input your surname: ");
+//        String surname = scanner.nextLine();
+//
+//        System.out.println("Input your age: ");
+//        int age = scanner.nextInt();
+//
+//        System.out.println("Hello my name is " + name + " " + surname + " and I am " + age + " years old. I am attending IT & Data Academy 2024!");
 
-        System.out.println("Input your surname: ");
-        String surname = scanner.nextLine();
 
-        System.out.println("Input your age: ");
-        int age = scanner.nextInt();
 
-        System.out.println("Hello my name is " + name + " " + surname + " and I am " + age + " years old. I am attending IT & Data Academy 2024!");
+        char ch = 'H';
+        byte by = 3;
+        int in = 11;
+        short sh = 0;
+        String st = "w0r1d";
+        float fl = 2.0f;
+        boolean bo = true;
+
+        String strBy = Integer.toString(by);
+
+        System.out.println(ch + strBy + in + sh + " " + st + " " + fl + " "+ bo);
     }
 
 }
+
+//    boolean
+//    byte
+//    char
+//    int
+//    long
+//    short
+
+
 
 
