a400d7b62e536d8d7a048b965d7f598700fa5f35
Edvinas <edvinas.gerdvila@mif.stud.vu.lt>
2024-03-05 15:59:37 +0200

    Task 6 is done

--- Backend/src/main/java/lt/itacademy/java/basics/Operators.java
+++ Backend/src/main/java/lt/itacademy/java/basics/Operators.java
@@ -7,15 +7,16 @@ import java.util.*;
 public class Operators {
 
 
-
     public static void main(String[] args) {
+
+        //// Task #5
 
         Scanner scanner = new Scanner(System.in);
 
-        Dictionary<Double,String> string_dict= new Hashtable<>();
-        string_dict.put(1.0,"Equilateral");
-        string_dict.put(2.0,"Isosceles");
-        string_dict.put(3.0,"Scalene");
+        Dictionary<Double, String> stringDict = new Hashtable<>();
+        stringDict.put(1.0, "Equilateral");
+        stringDict.put(2.0, "Isosceles");
+        stringDict.put(3.0, "Scalene");
 
         ArrayList<Double> Triangle = new ArrayList<>();
 
@@ -23,21 +24,51 @@ public class Operators {
         Triangle.add(Double.valueOf(scanner.next()));
         Triangle.add(Double.valueOf(scanner.next()));
 
-        double string_dict_key = determineTriangleType(Triangle);
+        double stringDictKey = determineTriangleType(Triangle);
 
-        System.out.println("Triangle is : " + string_dict.get(string_dict_key));
+        System.out.println("Triangle is : " + stringDict.get(stringDictKey));
+
+
+
+        //// Task #6
+        System.out.println("Please enter distance, current_fuel, fuel_usage, price_per_liter");
+
+
+        double distance = Double.parseDouble(scanner.next());
+        double currentFuel = Double.parseDouble(scanner.next());
+        double fuelUsage = Double.parseDouble(scanner.next());
+        double pricePerLiter = Double.parseDouble(scanner.next());
+
+        determineIsPossible(distance,currentFuel,fuelUsage,pricePerLiter);
 
         scanner.close();
 
     }
-    public static double determineTriangleType(ArrayList<Double> triangle_values){
+
+    public static double determineTriangleType(ArrayList<Double> triangle_values) {
 
 
         double num1 = Collections.frequency(triangle_values, triangle_values.get(0));
         double num2 = Collections.frequency(triangle_values, triangle_values.get(1));
         double num3 = Collections.frequency(triangle_values, triangle_values.get(2));
 
-        return Math.max(num1,Math.max(num2,num3));
+        return Math.max(num1, Math.max(num2, num3));
     }
 
+    public static void determineIsPossible(double distance, double currentFuel, double fuelUsage, double pricePerLiter) {
+
+        double totalFuelUsage = distance / 100 * fuelUsage;
+        currentFuel = currentFuel - totalFuelUsage;
+
+        if (currentFuel < 0) {
+
+            double fuel_price = (currentFuel * -1) * pricePerLiter;
+            System.out.printf("Destination is in %f. Car is not able to reach the destination. It needs %f liter of fuel more. It will cost %f ", distance, currentFuel*-1, fuel_price);
+
+        } else {
+
+            System.out.printf("Destination is in %f. Car is able to reach the destination. It has %f liters of fuel left.", distance, currentFuel);
+
+        }
+    }
 }
