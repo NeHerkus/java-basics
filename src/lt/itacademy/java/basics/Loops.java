d01331a2f1530d9ca4b183194a5bed2141af0471
urte.nistirenko@gmail.com <urte.nistirenko@gmail.com>
2024-03-05 20:21:15 +0200

    8 task

--- src/lt/itacademy/java/basics/Loops.java
+++ src/lt/itacademy/java/basics/Loops.java
@@ -8,13 +8,32 @@ public class Loops {
         int hours = 0;
         int minutes = 0;
 
-        for (int i = 1; i <= daysPerYear; i++) {
-            for (int hrs = 0; hrs < 24; hrs++) {
-                hours++;
-                for (int mins = 0; mins < 60; mins++) {
+//        for (int i = 1; i <= daysPerYear; i++) {
+//            for (int hrs = 0; hrs < 24; hrs++) {
+//                hours++;
+//                for (int mins = 0; mins < 60; mins++) {
+//                    minutes++;
+//                }
+//            }
+//        }
+//
+//        System.out.printf("""
+//                There are %s hours or %s minutes in one year.
+//                """, hours, minutes);
+
+        int days = 1;
+        while (days <= daysPerYear) {
+            int hrs = 0;
+            while (hrs < 24) {
+                hrs++;
+                int mins = 0;
+                while (mins < 60) {
+                    mins++;
                     minutes++;
                 }
+                hours++;
             }
+            days++;
         }
         System.out.printf("""
                 There are %s hours or %s minutes in one year.
