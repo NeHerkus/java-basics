9d26bc7b2bb2ddf0252f1c5ca7f87ad70b6e93ad
Lina <nematum@gmail.com>
2024-03-05 23:56:45 +0200

    11 task.

---
+++ src/lt/itacademy/java/basics/Art.java
@@ -0,0 +1,24 @@
+package lt.itacademy.java.basics;
+
+import java.util.Scanner;
+
+public class Art {
+    public static void main(String[] args) {
+        tree();
+    }
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void tree() {
+        System.out.println("How tall your tree will be. Write a natural number:");
+        int number = sc.nextInt();
+        for (int i = 1; i <= number * 2; i += 2) {
+            int padding = (number * 2 - i + 2) / 2;
+            System.out.printf("%" + padding + "s%s%" + padding + "s%n", "", "#".repeat(i), "");
+            if (i == number * 2 - 1) {
+                int pad = (number * 2 + 1) / 2;
+                System.out.printf("%" + pad + "s%s%" + pad + "s%n", "", "#", "");
+            }
+        }
+    }
+}
