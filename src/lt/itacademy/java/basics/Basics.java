370574cb21b21a3317e07662b71a6817b077e995
pijusv <pijus.vose@gmail.com>
2024-03-05 14:27:34 +0200

    Task 2 completed

--- src/lt/itacademy/java/basics/Basics.java
+++ src/lt/itacademy/java/basics/Basics.java
@@ -6,6 +6,30 @@ public class Basics
 {
     public static void main(String args[])
     {
+        // DoTaskOne();
+        var result = DoTaskTwo();
+
+        System.out.println(result);
+    }
+
+    private static String DoTaskTwo()
+    {
+        char firstVar = 'H';
+        int secondVar = 3;
+        byte thirdVar = 110;
+        char w = 'w';
+        short fourthVar = 0;
+        char idkVar = 'r';
+        int otherVal = 1;
+        char testVal = 'd';
+        float fifthVar = 2.0f;
+        boolean sixthVar = true;
+
+        return String.valueOf(firstVar) + String.valueOf(secondVar) + String.valueOf(thirdVar) + " " + String.valueOf(w) + String.valueOf(fourthVar) + String.valueOf(idkVar) + String.valueOf(otherVal) + String.valueOf(testVal) + " " + String.valueOf(fifthVar) + " " + String.valueOf(sixthVar);
+    }
+
+    private static void DoTaskOne()
+    {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your name:");
