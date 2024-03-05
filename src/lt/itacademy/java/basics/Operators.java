0fbe3b41b3e1bc915f4288483f09f58fc6be85d1
Oskaras-coder <osafinas@gmail.com>
2024-03-05 15:25:18 +0200

    6th task

--- src/lt/itacademy/java/basics/Operators.java
+++ src/lt/itacademy/java/basics/Operators.java
@@ -1,6 +1,7 @@
 package lt.itacademy.java.basics;
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Operators {
     public static void main(String[] args) {
@@ -10,6 +11,29 @@ public class Operators {
         int secondSide = sc.nextInt();
         int thirdSide = sc.nextInt();
         System.out.println(calculateTriangle(firstSide, secondSide, thirdSide) + firstSide + " " + secondSide + " "  + thirdSide);
+
+//        Task 6
+
+        double distance = sc.nextDouble();
+        double fuelInCar = sc.nextDouble();
+        double fuelUsage = sc.nextDouble();
+        double fuelPricePerLiter = sc.nextDouble();
+
+        boolean isAble = isAbleToDrive(distance, fuelInCar, fuelUsage);
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+        if(isAble){
+            double fuelLeft = Double.parseDouble(decimalFormat.format(fuelInCar - distance * fuelUsage / 100));
+            System.out.println("Destination is in " + distance + "km. Car is able to reach the destination. It will have " +
+                    fuelLeft + " liters of fuel left.");
+        }
+        else {
+            double fuelNeeded = Double.parseDouble(decimalFormat.format( distance * fuelUsage / 100 - fuelInCar));
+            double fuelNeededCost = Double.parseDouble(decimalFormat.format(fuelNeeded * fuelPricePerLiter));
+            System.out.println("Destination is in " + distance + "km. Car is not able to reach the destination. It needs " +
+                    fuelNeeded + " liters of fuel more. " + "It will cost " + fuelNeededCost + "." );
+        }
+
     }
 
     public static String calculateTriangle(int firstSide, int secondSide, int thirdSide){
@@ -27,6 +51,11 @@ public class Operators {
             return "Triangle is Isosceles. Sides: ";
         }
         else {return "Triangle is Scalene. Sides: ";}
+    }
+
+    public static boolean isAbleToDrive(double distance, double fuelInCar, double fuelUsage){
+        double carFuelUsage = distance * fuelUsage / 100;
+        return fuelInCar >= carFuelUsage;
     }
 }
 
