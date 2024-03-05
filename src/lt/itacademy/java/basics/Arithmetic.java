87434cbe16f2762b18cf81b83a989de94f8f9e2e
Lina <nematum@gmail.com>
2024-03-05 15:12:42 +0200

    4 task.

--- src/lt/itacademy/java/basics/Arithmetic.java
+++ src/lt/itacademy/java/basics/Arithmetic.java
@@ -12,9 +12,21 @@ public class Arithmetic {
         System.out.println("Cube's perimeter: " + length * 3 * 4 + "cm");
     }
 
-    public static void main(String[] args) {
+    public static void convertToCentimeters() {
+        System.out.println("Provide number");
+        String st = sc.nextLine();
+        String[] numbers = st.split(" ");
+        float feet = Float.parseFloat(numbers[0]);
+        float inches = Float.parseFloat(numbers[1]);
+        double centimeters = (feet * 12 + inches) * 2.54;
+        System.out.println(feet + " feet and " + inches + " inches = " + String.format("%.3f", centimeters) + " cm");
 
-        cube();
+
+    }
+
+    public static void main(String[] args) {
+//        cube();
+        convertToCentimeters();
 
     }
 }
