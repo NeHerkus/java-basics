65345eee4255ea16c55d069049451612c707fb58
Ugnius Motiejunas <motiejunasugnius@gmail.com>
2024-03-05 15:41:47 +0200

    Task 8

--- Basics/src/lt/itacademy/java/basics/Loops.java
+++ Basics/src/lt/itacademy/java/basics/Loops.java
@@ -2,20 +2,35 @@ package lt.itacademy.java.basics;
 
 public class Loops {
     public static void main(String[] args) {
-    hoursAndMinutes(365);
+    hoursAndMinutesFor(365);
+    hoursAndMinutesWhile(365);
     }
-    public static void hoursAndMinutes(int days) {
+
+    public static void hoursAndMinutesFor(int days) {
         if (days < 0) {
             System.out.println("Invalid Value");
         }
         int minutes = 0;
         int hours = 0;
-        for(int i = 0 ; i < days; i++)
-        {
+        for (int i = 0; i < days; i++) {
             minutes = minutes + 24 * 60;
             hours = hours + 24;
         }
         System.out.println("There are " + hours + " hours or " + minutes + " minutes in one year.");
-        }
     }
+    public static void hoursAndMinutesWhile(int days){
+        if (days < 0) {
+            System.out.println("Invalid Value");
+        }
+        int minutes = 0;
+        int hours = 0;
+        int i = 0;
+        while (i < days) {
+            minutes = minutes + 24 * 60;
+            hours = hours + 24;
+            i++;
+        }
+        System.out.println("There are " + hours + " hours or " + minutes + " minutes in one year.");
+    }
+}
 
