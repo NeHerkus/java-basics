1967c68a9773fee062efa2fdc0fbdd79f528ee35
Gabija Sabeckyte <gabeckyte@gmail.com>
2024-03-05 16:02:02 +0200

    task 4

--- src/lt/itacademy/java/basics/Arithmetic.java
+++ src/lt/itacademy/java/basics/Arithmetic.java
@@ -9,6 +9,17 @@ public class Arithmetic {
         System.out.println("Cubes volume: " + volume + "cm3");
         System.out.println("Cubes perimeter: " + perimeter + "cm");
 
+        double feet = 2.4;
+        double inches = 4.0;
+
+        double convertInchToCm = inches * 2.54;
+        double convertFeetToCm = (feet * 12) * 2.54;
+        double total = convertInchToCm + convertFeetToCm;
+
+        System.out.println(feet + " feet and " + inches + " inches = " + total + " cm");
+
+
+
 
     }
 }
