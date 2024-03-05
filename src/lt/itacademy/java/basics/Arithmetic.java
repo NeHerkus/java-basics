cb35c7ab076cbb522e1d383d8f651cebee321d40
Oskaras-coder <osafinas@gmail.com>
2024-03-05 14:29:54 +0200

    4th task done

--- src/lt/itacademy/java/basics/Arithmetic.java
+++ src/lt/itacademy/java/basics/Arithmetic.java
@@ -6,10 +6,22 @@ public class Arithmetic {
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
+
+//        Third task
         double number = sc.nextDouble();
         double volume = Math.pow(number, 3);
         double perimeter = number * 12;
         System.out.println("Cube’s volume: " + (int)volume + "cm3");
         System.out.println("Cube’s perimeter: " + (int)perimeter + "cm");
+
+//        Fourth task
+        double feet = sc.nextDouble();
+        double inches = sc.nextDouble();
+        System.out.println(feet + " feet and " + inches + " inches = " + convertInchesToCentimetres(feet, inches) + " cm");
+    }
+    public static double convertInchesToCentimetres(double feet, double inches){
+        double convertedNumber;
+        inches += 12 * feet;
+        return (inches * 2.54);
     }
 }
--- src/lt/itacademy/java/basics/Basics.java
+++ src/lt/itacademy/java/basics/Basics.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Basics {
     public static void main(String[] args){
-
+//      First task
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
         String surname = sc.nextLine();
@@ -12,6 +12,7 @@ public class Basics {
 
         System.out.println("Hello my name is " + name + " " + surname + " and I am " + years + " a years old. I am attending IT & Data Academy 2024!");
 
+//      Second task
         String hello = "H3110 w0r1d ";
         float number = 2.0f;
         boolean yes = true;
