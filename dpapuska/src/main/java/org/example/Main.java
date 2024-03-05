ba778fd77a7453b97dac4b359d7f7394cd69b5b2
dzeivis <deividaspapuska@gmail.com>
2024-03-05 14:22:14 +0200

    task1

---
+++ dpapuska/src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name, surname and age.");
+
+        String input = scanner.nextLine();
+        String[] inputValues = input.split("\\s+");
+        String name = inputValues[0];
+        String surname = inputValues[1];
+        int age = Integer.parseInt(inputValues[2]);
+
+        if(inputValues.length >= 3) {
+            System.out.println("Hello my name is " + name + " " + surname + " and I am " + age + " years old. I am attending IT & Data\n" + "Academy 2024!");
+        }
+
+        int let0 = 3110;
+        char let1 = 'H';
+        char let2 = 'w';
+        int let3 = 0;
+        char let4 = 'r';
+        int let5 = 1;
+        char let6 = 'd';
+        float let7 = 2.0f;
+        boolean let8 = true;
+
+        System.out.println(let1 + "" + let0 + " " + let2 + let3 + let4 + let5 + let6 + " " + let7 + " " + let8);
+
+    }
+}
