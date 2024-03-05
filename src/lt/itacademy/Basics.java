7667a8b6da90a4dd4c336327a38f1210d94b451f
Rychka <@ryckutis>
2024-03-05 14:04:52 +0200

    first

--- src/lt/itacademy/Basics.java
+++ src/lt/itacademy/Basics.java
@@ -4,18 +4,26 @@ import java.util.Scanner;
 
 public class Basics {
     public static void main(String[] args) {
+        System.out.println(firstTaskResult());
+
+    }
+
+    //-- 1 task --
+    public static String firstTaskResult(){
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter your name: ");
-        String name = scanner.toString();
+        String name = scanner.next();
 
         System.out.print("Enter your surname: ");
-        String surname = scanner.toString();
+        String surname = scanner.next();
 
-        System.out.println("Enter your age: ");
+        System.out.print("Enter your age: ");
         int age = scanner.nextInt();
 
-        String result = "Hello my name is " + name + " " + surname + " and I am " + age + "years old. I am attending IT & Data Academy 2024!";
-        System.out.println(result);
+        String result = "Hello my name is " + name + " " + surname + " and I am " + age + " years old. I am attending IT & Data Academy 2024!";
+
+        return result;
     }
 
 }
