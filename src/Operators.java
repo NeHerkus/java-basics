4dfb865524f35ed2b4afa73e7e6d01e19d9f3471
nojusbuc <noahmb.nmb@gmail.com>
2024-03-05 15:51:51 +0200

    task 6 complete

--- src/Operators.java
+++ src/Operators.java
@@ -4,11 +4,17 @@ public class Operators {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float side1 = scanner.nextFloat(),
-                side2 = scanner.nextFloat(),
-                side3 = scanner.nextFloat();
-        determineTriangleType(side1, side2, side3);
-        System.out.printf("Sides: %.2f %.2f, %.2f", side1, side2, side3);
+//        task 5
+//        float side1 = scanner.nextFloat(),
+//                side2 = scanner.nextFloat(),
+//                side3 = scanner.nextFloat();
+//        determineTriangleType(side1, side2, side3);
+//        System.out.printf("Sides: %.2f %.2f, %.2f", side1, side2, side3);
+
+
+//        task 6
+        float destination = scanner.nextFloat(), fuel = scanner.nextFloat(), fuelUsage100km = scanner.nextFloat(), fuelLiterPrice = scanner.nextFloat();
+        fuelConsumptionAndCost(destination, fuel, fuelUsage100km, fuelLiterPrice);
 
     }
 
@@ -21,5 +27,23 @@ public class Operators {
         } else {
             System.out.println("triangle is scalene");
         }
+    }
+
+    static void fuelConsumptionAndCost(
+            float distance,
+            float fuel, float fuelUsage100km, float fuelLiterPrice) {
+        float maxDistance = fuel * 100f /fuelUsage100km;
+        System.out.println(maxDistance);
+        if (maxDistance >= distance) {
+            float excessFuel = (maxDistance - distance) / 100f * fuelUsage100km;
+            System.out.printf("Destination is in %.2f km. Car will reach destination. It will have %.2f liters of fuel left. \n", distance, excessFuel);
+        } else {
+            float extraFuelNeeded = (distance - maxDistance) / 100f * fuelUsage100km;
+            float extraCost = extraFuelNeeded * fuelLiterPrice;
+            System.out.printf("Destination is in %.2f km. " +
+                    "Car will not reach destination. It needs %.2f liters of fuel more. " +
+                    "It will cost %.2f. \n", distance, extraFuelNeeded, extraCost);
+        }
+
     }
 }
