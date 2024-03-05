07e8a97cd478adafaa52debc9b55fe1a2b287536
Gabija Sabeckyte <gabeckyte@gmail.com>
2024-03-05 14:26:21 +0200

    task 2

--- src/lt/itacademy/java/basics/Basics.java
+++ src/lt/itacademy/java/basics/Basics.java
@@ -5,7 +5,15 @@ public class Basics {
 
         String name = "Gabija";
         String surname = "Sabeckyte";
+        byte lbyte = 3;
         int age = 23;
+        short lshort = 1;
+        float lfloat = 1.0f;
+        char lchar = 'G';
+        boolean lbool = true;
+        String lOutput = lchar+lbyte+lshort+age+name+' '+lbool;
+        System.out.println(lOutput);
+
         System.out.println("Hello my name is " + name + " " + surname + " and I'm " + age + " years old. I am attending IT & Data\n" +
                 "Academy 2024!");
 
