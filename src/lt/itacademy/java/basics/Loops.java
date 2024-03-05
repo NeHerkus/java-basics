fe8f8e675e72dc569adaec2f4fd75e37e1689662
Ugnė Dambrauskė <u.dambrauske@gmail.com>
2024-03-05 21:29:46 +0200

    task 9

--- src/lt/itacademy/java/basics/Loops.java
+++ src/lt/itacademy/java/basics/Loops.java
@@ -20,6 +20,7 @@ public class Loops {
 //        return "There are " + hours + " hours or " + minutes + " minutes in one year.";
 //    }
 
+
     static String convertDays2 (int days) {
          int hours = 0;
          int minutes = 0;
@@ -34,6 +35,16 @@ public class Loops {
         return "There are " + hours + " hours or " + minutes + " minutes in one year.";
     }
 
+    static String calculateFactorial(int number) {
+        int factorial = 1;
+
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+
+        return String.format("Factorial of number %d is %d", number, factorial);
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -41,5 +52,9 @@ public class Loops {
         System.out.println("Please enter a number of days");
         int input = scanner.nextInt();
         System.out.println(convertDays2(input));
+
+        System.out.println("Please enter an integer");
+        int integerInput = scanner.nextInt();
+        System.out.println(calculateFactorial(integerInput));
     }
 }
