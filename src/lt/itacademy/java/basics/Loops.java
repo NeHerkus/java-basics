0979e5013dda62bfcb9dab17cd8da4ac4f83ec48
ufuksaylan <ufuksaylannn@gmail.com>
2024-03-05 16:20:44 +0200

    add task 8

--- src/lt/itacademy/java/basics/Loops.java
+++ src/lt/itacademy/java/basics/Loops.java
@@ -7,18 +7,31 @@ public class Loops {
   public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
 
+//    Task 7
     System.out.println("provide days");
 
     int days = reader.nextInt();
+//
+//    int hours = 0;
+//    int minutes = 0;
+//    for (int i = 0; i < days; i++) {
+//      hours+=24;
+//      minutes+=24*60;
+//    }
+//
+//    System.out.println("There are " + hours + " hours or " + minutes + " in one year.");
 
+//    Task 8
     int hours = 0;
     int minutes = 0;
-    for (int i = 0; i < days; i++) {
+    int i = 0;
+    while (i < days) {
       hours+=24;
       minutes+=24*60;
+      i++;
     }
-
     System.out.println("There are " + hours + " hours or " + minutes + " in one year.");
+
   }
 
   }
