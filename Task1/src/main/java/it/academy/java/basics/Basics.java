b97af1d51d4fc9434a4ca92f9254e7cf25ea0173
Eimantas <eimantas.dziovenas@gmail.com>
2024-03-05 13:44:20 +0200

    initial commit

--- Task1/src/main/java/it/academy/java/basics/Basics.java
+++ Task1/src/main/java/it/academy/java/basics/Basics.java
@@ -1,2 +1,15 @@
-package it.academy.java.basics;public class Basics {
+package it.academy.java.basics;
+
+import java.util.Scanner;
+
+class Basics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        String surname = scanner.nextLine();
+        int age = scanner.nextInt();
+        System.out.printf("Hello my name is %s %s and I am %d years old. I am attending IT & Data\n" +
+                "Academy 2024!", name, surname, age);
+    }
 }
