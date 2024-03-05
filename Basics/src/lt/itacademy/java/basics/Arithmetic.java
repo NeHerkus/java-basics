696b74283637cae1405bbe9e7968990868ef57a5
Ugnius Motiejunas <motiejunasugnius@gmail.com>
2024-03-05 14:56:36 +0200

    Task 4

--- Basics/src/lt/itacademy/java/basics/Arithmetic.java
+++ Basics/src/lt/itacademy/java/basics/Arithmetic.java
@@ -1,2 +1,34 @@
-package lt.itacademy.java.basics;public class Arithmetic {
+package lt.itacademy.java.basics;
+
+import java.util.Scanner;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        //create a scanner for inputing values
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter the length of the cube: ");
+        int length = myObj.nextInt();
+        volume(length);
+        perimeter(length);
+
+        System.out.println("Enter the feet: ");
+        float feet = myObj.nextFloat();
+        System.out.println("Enter the inch: ");
+        float inch = myObj.nextFloat();
+        converter(feet, inch);
+    }
+
+    private static void volume(int length) {
+        int volume = length * length * length;
+        System.out.println(volume + "cm3");
+    }
+    private static void perimeter(int length) {
+        int perimeter = length * 12;
+        System.out.println(perimeter + "cm");
+    }
+    private static void converter(float feet, float inch) {
+        float cm = (float) ((feet * 30.48) + (inch * 2.54));
+        System.out.println(String.format("%.3fcm", cm) + "cm");
+    }
 }
+
--- Basics/src/lt/itacademy/java/basics/Basics.java
+++ Basics/src/lt/itacademy/java/basics/Basics.java
@@ -4,19 +4,27 @@ import java.util.Scanner;
 
 public class Basics {
     public static void main(String[] args) {
+        secondTask();
         Scanner myObj = new Scanner(System.in);
-
         String userName = myObj.nextLine();
         String lastName = myObj.nextLine();
         Integer age = myObj.nextInt();
+        firstTask(userName, lastName, age);
+    }
 
+    private static void firstTask(String userName, String lastName, Integer age) {
         System.out.println("Hello my name is " + userName + " " + lastName + " and I am " + age + " years old. I am attending IT & Data Academy 2024");
+    }
 
-        int a = 3110;
-        char b = 'B';
-        double c = 2.0;
+    private static void secondTask() {
+        int a = 3;
+        char b = 'H';
+        float c = 2.0F;
         boolean d = true;
+        char[] e = "w0r1d".toCharArray();
+        byte f = 110;
 
-
+        String result = b + "" + a + "" + f + " " + new String(e) + " " + c + " " + d;
+        System.out.println(result);
     }
 }
