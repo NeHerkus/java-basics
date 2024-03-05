7b61d31cf3b47566530a5d73ea37492c37e30e73
Patricija Katinaite <patricija.katinaite@gmail.com>
2024-03-05 14:45:53 +0200

    Task 2

--- src/lt/itacademy/java/basics/Basics.java
+++ src/lt/itacademy/java/basics/Basics.java
@@ -4,6 +4,7 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
 
+        // TASK 1
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter your name: ");
@@ -15,6 +16,22 @@ class Main {
         System.out.print("Enter your age: ");
         int age = sc.nextInt();
 
-        System.out.print("Hello my name is " + name + " " + surname + " and I am " + age + " years old. I am attending IT & Data Academy 2024!");
+        System.out.println("Hello my name is " + name + " " + surname + " and I am " + age + " years old. I am attending IT & Data Academy 2024!");
+
+        // TASK 2
+
+        int intas = 31;
+        byte byteas = 10;
+        String stringas = "w0r1d";
+        char charas = 'H';
+        boolean boolas = true;
+        float floatas = 2.0f;
+
+        System.out.println("" + charas + intas + byteas + " " + stringas + " " + floatas + " " + boolas);
+
+
+
+
+
     }
 }
