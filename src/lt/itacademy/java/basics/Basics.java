1281f4403734edd7c02e1041c9199032d5712895
Gabija Sabeckyte <gabeckyte@gmail.com>
2024-03-05 14:39:11 +0200

    task 2

--- src/lt/itacademy/java/basics/Basics.java
+++ src/lt/itacademy/java/basics/Basics.java
@@ -3,17 +3,19 @@ package lt.itacademy.java.basics;
 public class Basics {
     public static void main(String[] args) {
 
+        byte lbyte = 3;
+        short lshort = 1;
+        int lint = 1;
+        float lfloat = 2.0f;
+        char lchar = 'H';
+        boolean lbool = true;
+        String lstring = " w0r1d ";
+        // String task2 = lchar+lbyte+lshort+lint+lstring+lfloat +' '+lbool;
+        System.out.println(lchar+lbyte+lshort+lint+lstring+lfloat +' '+lbool);
+
         String name = "Gabija";
         String surname = "Sabeckyte";
-        byte lbyte = 3;
         int age = 23;
-        short lshort = 1;
-        float lfloat = 1.0f;
-        char lchar = 'G';
-        boolean lbool = true;
-        String lOutput = lchar+lbyte+lshort+lfloat+age+name+' '+lbool;
-        System.out.println(lOutput);
-
         System.out.println("Hello my name is " + name + " " + surname + " and I'm " + age + " years old. I am attending IT & Data\n" +
                 "Academy 2024!");
 
