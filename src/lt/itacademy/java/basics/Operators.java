9cc81dc8a371a5bd57cdf04de2656568a0f5316c
urte.nistirenko@gmail.com <urte.nistirenko@gmail.com>
2024-03-05 19:25:45 +0200

    5 task finished

--- src/lt/itacademy/java/basics/Operators.java
+++ src/lt/itacademy/java/basics/Operators.java
@@ -8,10 +8,19 @@ public class Operators {
         int secMeas = scanner.nextInt();
         int thirdMeas = scanner.nextInt();
 
+        if (firstMeas <= 0 || secMeas <= 0 || thirdMeas <= 0 ||
+                firstMeas + secMeas <= thirdMeas || firstMeas + thirdMeas <= secMeas || secMeas + thirdMeas <= firstMeas) {
+            System.out.println("Triangle is invalid. Sides: " + firstMeas + " " + secMeas + " " + thirdMeas);
+        } else if (firstMeas == secMeas && secMeas == thirdMeas) {
+            System.out.println("Triangle is Equilateral. Sides: " + firstMeas + " " + secMeas + " " + thirdMeas);
+        } else if (firstMeas == secMeas || secMeas == thirdMeas || firstMeas == thirdMeas) {
+            System.out.println("Triangle is Isosceles. Sides: " + firstMeas + " " + secMeas + " " + thirdMeas);
+        } else {
+            System.out.println("Triangle is Scalene. Sides: " + firstMeas + " " + secMeas + " " + thirdMeas);
+        }
 
-
-//        System.out.printf("""
-//                Triangle is Scalene. Sides: %s %s %s
-//                """, firstMeas, secMeas, thirdMeas);
+        System.out.printf("""
+                Triangle is Scalene. Sides: %s %s %s
+                """, firstMeas, secMeas, thirdMeas);
     }
 }
