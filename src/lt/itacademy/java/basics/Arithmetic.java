3bd1af368e7f1657e0fecada4f7c501ccbf7548d
ufuksaylan <ufuksaylannn@gmail.com>
2024-03-05 14:42:57 +0200

    add task 4

--- src/lt/itacademy/java/basics/Arithmetic.java
+++ src/lt/itacademy/java/basics/Arithmetic.java
@@ -3,7 +3,21 @@ package lt.itacademy.java.basics;
 import java.util.Scanner;
 
 public class Arithmetic {
+
+  public static void convertFeetAndInchToCm(double feet, double inches) {
+
+    double inchToCm = inches * 2.54;
+
+    double footToCm = 12 * 2.54 * feet;
+
+    double totalCm = inchToCm + footToCm;
+
+    System.out.println(feet + " feet and " + inches + " inches = " + totalCm + " cm");
+  }
+
   public static void main(String[] args) {
+
+//    Task 3
     Scanner reader = new Scanner(System.in);
 
     System.out.println("Enter edge value: ");
@@ -17,6 +31,16 @@ public class Arithmetic {
     System.out.println("Cube’s volume: " + volume + "cm3");
     System.out.println("Cube’s perimeter: " + perimeter + "cm");
 
+
+
+//    Task 4
+    System.out.println("enter feet");
+    double feet = reader.nextDouble();
+
+    System.out.println("enter inch");
+    double inch = reader.nextDouble();
+
+    convertFeetAndInchToCm(feet, inch);
 
 
   }
