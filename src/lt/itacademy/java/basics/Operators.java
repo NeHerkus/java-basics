d37d11fdc5e4c83f983a2e9a172554df65a02a8d
urte.nistirenko@gmail.com <urte.nistirenko@gmail.com>
2024-03-05 16:58:30 +0200

    5 task

---
+++ src/lt/itacademy/java/basics/Operators.java
@@ -0,0 +1,17 @@
+package lt.itacademy.java.basics;
+import java.util.Scanner;
+public class Operators {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter triangle measurements: ");
+        int firstMeas = scanner.nextInt();
+        int secMeas = scanner.nextInt();
+        int thirdMeas = scanner.nextInt();
+
+
+
+//        System.out.printf("""
+//                Triangle is Scalene. Sides: %s %s %s
+//                """, firstMeas, secMeas, thirdMeas);
+    }
+}
