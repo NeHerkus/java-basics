ae73cf2e47a67551d87acd8ec95eab3f8ffa8c2b
Ingrida <ingridarrr@gmail.com>
2024-03-05 20:58:42 +0200

    ninth task

--- src/lt/itacademy/java/basics/Loops.java
+++ src/lt/itacademy/java/basics/Loops.java
@@ -10,7 +10,11 @@ public class Loops {
         System.out.println("How many days per year to calculate? ");
         int days = scanner.nextInt();
 
+        System.out.println("Number to calculate factorial: ");
+        int number = scanner.nextInt();
+
         calculate(days);
+        calculateFactorial(number);
     }
 
     public static void calculate(int days) {
@@ -24,5 +28,13 @@ public class Loops {
             i++;
         }
         System.out.println("There are " + hours + " hours or " + minutes + " minutes in one year.");
+    }
+
+    public static void calculateFactorial(int number) {
+        int product = 1;
+        for (int i = 1; i <= number; i++) {
+            product *= i;
+        }
+        System.out.println("Factorial of number " + number + " is " + product + ".");
     }
 }
