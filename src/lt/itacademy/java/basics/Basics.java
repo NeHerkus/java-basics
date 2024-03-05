3ad70353b8944810de1d410d2567d4ceaf3465aa
Lina <nematum@gmail.com>
2024-03-05 14:23:36 +0200

    Hello 1 task correction

--- src/lt/itacademy/java/basics/Basics.java
+++ src/lt/itacademy/java/basics/Basics.java
@@ -7,11 +7,10 @@ public class Basics {
     static Scanner sc = new Scanner(System.in);
 
     public static void read() {
-        System.out.println("Write your name and surname");
-        String name = sc.next();
-        System.out.println("Age");
-        int age = sc.nextInt();
-        System.out.println("Hello name is " + name + " and I am " + age + " years old. I am attending IT & Data Academy 2024!");
+        System.out.println("Write your name and surname anf age");
+        String st = sc.nextLine();
+        String[] strings = st.split(" ");
+        System.out.println("Hello name is " + strings[0] + " " + strings[1] + " and I am " + strings[2] + " years old. I am attending IT & Data Academy 2024!");
     }
 
     public static void main(String[] args) {
