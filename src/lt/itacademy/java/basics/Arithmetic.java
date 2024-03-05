7d110b7afee25c66e1b63873d1e9c370801cb6ad
Martynas Piliutis <martynas.piliutis@gmail.com>
2024-03-05 20:33:21 +0200

    MPiliutis Task4

--- src/lt/itacademy/java/basics/Arithmetic.java
+++ src/lt/itacademy/java/basics/Arithmetic.java
@@ -1,4 +1,5 @@
 package lt.itacademy.java.basics;
+import java.util.Arrays;
 import java.util.Scanner;
 import static java.lang.System.*;
 public class Arithmetic {
@@ -8,5 +9,14 @@ public class Arithmetic {
         int edge = scanner.nextInt();
         out.println("Cube's volume: " + (int) Math.pow(edge,3) + "cm3");
         out.println("Cube's perimeter: " + edge * 12 + "cm");
+
+        Scanner scan = new Scanner(in);
+        out.print("Enter feet and inches: ");
+        String imp = scan.nextLine();
+        String[] impArr = imp.split(" ");
+
+        double feet = Double.parseDouble(impArr[0]);
+        double inch = Double.parseDouble(impArr[1]);
+        out.println(feet + " feet and " + inch + " inches = " + (12 * feet + inch)*2.54 + "cm");
     }
 }
