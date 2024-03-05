e5844f5f85b0ac749da8b686f7585d03b900d713
Darius <mlecka.darius@gmail.com>
2024-03-05 16:47:25 +0200

    task6

--- src/lt/itacademy/java/basics/Operators.java
+++ src/lt/itacademy/java/basics/Operators.java
@@ -22,14 +22,24 @@ public class Operators {
 //        Program has four parameters: distance to the destination,
 //        how much fuel does the car have in the tank, car fuel usage per 100km and fuel price per liter.
 
+        System.out.println("Enter distance to the destination: ");
+        int distanceToDestination = Integer.parseInt(scan.nextLine());
 
+        System.out.println("Enter how much fuel does the car have in the tank: ");
+        int fuelInTheTank = Integer.parseInt(scan.nextLine());
 
+        System.out.println("Enter car fuel usage per 100km: ");
+        int fuelUsagePer100km = Integer.parseInt(scan.nextLine());
 
+        System.out.println("Enter fuel price per liter: ");
+        double fuelPricePerLiter = Double.parseDouble(scan.nextLine());
+
+        driveToDestination(distanceToDestination,fuelInTheTank,fuelUsagePer100km,fuelPricePerLiter);
     }
 
     //task 5
     static String triangle(int firstSide, int secondSide, int thirdSide) {
-        if (firstSide + secondSide > thirdSide &&  secondSide + thirdSide >firstSide  && firstSide + thirdSide > secondSide) {
+        if (firstSide + secondSide > thirdSide && secondSide + thirdSide > firstSide && firstSide + thirdSide > secondSide) {
             if (secondSide == thirdSide && firstSide == thirdSide) {
                 return "Triangle is Equilateral. Sides: " + firstSide + " " + secondSide + " " + thirdSide;
             } else if (secondSide == thirdSide || firstSide == thirdSide || firstSide == secondSide) {
@@ -40,7 +50,20 @@ public class Operators {
         }
         return "Triangle is invalid. Sides: " + firstSide + " " + secondSide + " " + thirdSide;
     }
-    //task6
 
+    //task6
+    static void driveToDestination(int distanceToDestination, int fuelInTheTank, int fuelUsagePer100km, double fuelPricePerLiter) {
+        double fuelLeft = fuelInTheTank - ((double) (distanceToDestination * fuelUsagePer100km) /100);
+        System.out.println("liko kuro: " +fuelLeft);
+        if (fuelLeft>=0) {
+            System.out.println("Destination is in " + distanceToDestination + " km. Car is able to reach the destination. It will have "+ fuelLeft+" liters of\n" +
+                    "    fuel left.");
+
+        } else {
+            System.out.println("Destination is in  " + distanceToDestination + " km. Car is not able to reach the destination. It needs "+(-1)*fuelLeft+" liter of fuel\n" +
+                    "more. It will cost " + (-1)*fuelLeft*fuelPricePerLiter + ".");
+        }
+
+    }
 
 }
