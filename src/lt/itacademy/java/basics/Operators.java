74f17b1bc38954efff58c118747a7220a1e5fd75
Anhelina Velychko <avelichko469@gmail.com>
2024-03-05 16:10:42 +0200

    Task 6

--- src/lt/itacademy/java/basics/Operators.java
+++ src/lt/itacademy/java/basics/Operators.java
@@ -12,16 +12,51 @@ public class Operators {
     }
 
     static public void main(String[] args){
-        int a;
-        int b;
-        int c;
-
         Scanner scanner = new Scanner(System.in);
-        a = scanner.nextInt();
-        b = scanner.nextInt();
-        c = scanner.nextInt();
+//Task 5
+       analyzeTriangle(scanner);
+//Task 6
+        analizeCarRide(scanner);
+    }
 
-        Operators op = new Operators(a,b,c);
+    public static void analizeCarRide(Scanner scanner){
+        double distance;
+        double currFuel;
+        double fuelUsage;
+        double fuelPrice;
+        double requiredFuel;
+        System.out.println("Enter distance, current fuel amount, fuel usage for 100km, and price: ");
+
+        distance = Double.parseDouble(scanner.next());
+        currFuel = Double.parseDouble(scanner.next());
+        fuelUsage = Double.parseDouble(scanner.next());
+        fuelPrice = Double.parseDouble(scanner.next());
+        requiredFuel = distance*fuelUsage/100;
+
+        if(distance < (100* currFuel)/fuelUsage){
+            double leftFuel = currFuel-requiredFuel;
+            System.out.println("Destination is in "+ distance +
+                    ". Car is able to reach the destination. It will have "+ leftFuel +" liters of fuel left.");
+        }else {
+            double neededFuel = requiredFuel - currFuel;
+            double price = neededFuel*fuelPrice;
+            System.out.println("Destination is in " + distance
+                    + ". Car is not able to reach the destination. It needs "
+                    + neededFuel+ " liter of fuel more. It will cost " + price+ ".");
+        }
+
+    }
+
+    public static void analyzeTriangle(Scanner scanner){
+        int aSide;
+        int bSide;
+        int cSide;
+        System.out.println("Enter trinagle's sides: ");
+        aSide = scanner.nextInt();
+        bSide = scanner.nextInt();
+        cSide = scanner.nextInt();
+
+        Operators op = new Operators(aSide,bSide,cSide);
 
         if(!op.isValid()){
             System.out.println("Triangle is invalid. Sides : " + op.a + op.b + op.c);
@@ -33,6 +68,7 @@ public class Operators {
             System.out.println("Triangle is Scalene. Sides : " + op.a + op.b + op.c);
         }
     }
+
 
     public boolean isEquilateral(){
         return a==b && b==c;
