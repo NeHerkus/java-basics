d154bb7dd9f66b5c6f2f7df5c54cf5665cd2482e
GabrieleJasiunaite <jasinait.gabriel@gmail.com>
2024-03-05 15:24:51 +0200

    task 11

--- src/lt/itacademy/java/basics/Art.java
+++ src/lt/itacademy/java/basics/Art.java
@@ -1,4 +1,40 @@
 package lt.itacademy.java.basics;
 
+import java.util.Scanner;
+
 public class Art {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int symbols = 1;
+
+        for (int i = input - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= symbols; j++) {
+                System.out.print("#");
+            }
+            symbols += 2;
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        for (int j = 0; j <= input - 1; j++) {
+            System.out.print(" ");
+        }
+
+        System.out.print("#");
+
+        for (int j = 0; j <= input - 1; j++) {
+            System.out.print(" ");
+        }
+
+        System.out.println();
+    }
+
 }
