950210a5800d28c15b66b2b04e2ce7f94b278707
simzub <78310955+simzub@users.noreply.github.com>
2024-03-05 16:38:48 +0200

    add task 8

--- src/lt/itacademy/java/basics/Loops.java
+++ src/lt/itacademy/java/basics/Loops.java
@@ -3,17 +3,24 @@ package lt.itacademy.java.basics;
 public class Loops {
 
     public static void yearCalculator() {
+        int days = 365;
         int minutesInHour = 60;
         int hoursInDay = 24;
         int hours = 0;
         int minutes = 0;
+        int hoursCopy = 0;
 
-        for (int i = 0; i < 365; i++) {
+
+        while(days > 0) {
             hours += hoursInDay;
+            days--;
         }
 
-        for (int i = 0; i < hours; i++) {
+        hoursCopy = hours;
+
+        while(hoursCopy > 0) {
             minutes += minutesInHour;
+            hoursCopy--;
         }
 
         System.out.println("There are " + hours + " hours or " + minutes + " minutes in one year.");
