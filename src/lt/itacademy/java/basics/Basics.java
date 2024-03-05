3a4c2726d4893a0a9f05264ca3ef59aa295644e5
Martynas Piliutis <martynas.piliutis@gmail.com>
2024-03-05 15:43:23 +0200

    MPiliutis Task2

--- src/lt/itacademy/java/basics/Basics.java
+++ src/lt/itacademy/java/basics/Basics.java
@@ -1,14 +1,27 @@
 package lt.itacademy.java.basics;
-
 import java.util.Scanner;
+import static java.lang.System.*;
 
 public class Basics {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(in);
         String name = scanner.nextLine();
         String surname = scanner.nextLine();
         int age = scanner.nextInt();
-        System.out.println("Hello my name is " + name + " " + surname + " and I am " + age + " years old. I am attending IT & Data Academy 2024!");
+        out.println("Hello my name is " + name + " " + surname + " and I am " + age + " years old. I am attending IT & Data Academy 2024!");
+
+        boolean b = true;
+        byte o = 1;
+        char w = 'w';
+        char r = 'r';
+        char d = 'd';
+        char h = 'H';
+        char sp = ' ';
+        short s = 0;
+        float f = 2.0f;
+        int i = 3110;
+        String line = h + String.valueOf(i) + sp + w + String.valueOf(s) + r + String.valueOf(o) + d + sp + f + sp + b;
+        out.println(line);
     }
 }
