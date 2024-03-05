5da471db6fb789087de5b7c904d7c63a0f01a7f8
Gabija Sabeckyte <gabeckyte@gmail.com>
2024-03-05 20:29:21 +0200

    task 6

--- src/lt/itacademy/java/basics/Operators.java
+++ src/lt/itacademy/java/basics/Operators.java
@@ -10,6 +10,15 @@ public class Operators {
         String triangleType = triangleType(side1, side2, side3);
 
         System.out.println("Triangle is " + triangleType);
+
+        int distanceToDestination = 100;
+        int leftFuel = 6;
+        int fuelUsage = 7;
+        double fuelPrice = 1.25;
+
+        calculateFuel(distanceToDestination, leftFuel, fuelUsage, fuelPrice);
+
+
     }
 
             public static String triangleType(int side1, int side2, int side3) {
@@ -23,5 +32,20 @@ public class Operators {
             else
                 return "Scalene";
         }
+
+        public static void calculateFuel(int distanceToDestination, int leftFuel, int fuelUsage, double fuelPrice) {
+        double neededFuel = distanceToDestination / 100 * fuelUsage - leftFuel;
+        double cost = neededFuel * fuelPrice;
+
+        if (neededFuel <= 0) {
+            double fuelLeft = leftFuel - distanceToDestination / 100 * fuelUsage;
+            System.out.printf("Destination is in %d km. Car is able to reach the destination. It will have %.1f liters of fuel left.%n", distanceToDestination, leftFuel);
+        } else {
+            System.out.printf("Destination is in %d km. Car is not able to reach the destination. It needs %.1f liters of fuel more. It will cost %.2f.%n", distanceToDestination, neededFuel, cost);
+        }
+
+        }
+
+
     }
 
