913268827fdbfe96ffcc0af31d279d2cb54ac4d8
simzub <78310955+simzub@users.noreply.github.com>
2024-03-05 14:41:17 +0200

    add task 3

---
+++ src/lt/itacademy/java/basics/Arithmetic.java
@@ -0,0 +1,9 @@
+package lt.itacademy.java.basics;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        int side = 5;
+        System.out.println("Volume of cube: " + (int)Math.pow(side,3) + "cm3");
+        System.out.println("Perimeter of cube: " + side*12 + "cm" );
+    }
+}
