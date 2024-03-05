e0176f35d055507d4067a0d610e40c3e39122bca
EgleeB <117934410+EgleeB@users.noreply.github.com>
2024-03-05 15:24:35 +0200

    fourth task added

--- src/lt/itacademy/java/basics/Arithmetic.java
+++ src/lt/itacademy/java/basics/Arithmetic.java
@@ -4,15 +4,29 @@ import java.util.Scanner;
 
 public class Arithmetic {
 
-    public static void main(String[] args) {
+    public static double feetToCentimeters(double feet, double inches) {
+        double totalInches = (feet * 12) + inches;
+        double centimeters = totalInches * 2.54;
+        return centimeters;
+    }
 
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter side length of the cube: ");
         int sideLength = in.nextInt();
 
         int volume = sideLength * sideLength * sideLength;
         System.out.println("Cube’s volume: " + volume + "cm3");
         int perimeter = 12 * sideLength;
         System.out.println("Cube’s perimeter: " + perimeter + "cm");
-    }
 
+        System.out.print("Enter feet: ");
+        double feet = in.nextDouble();
+        System.out.print("Enter inches: ");
+        double inches = in.nextDouble();
+
+        double centimeters = feetToCentimeters(feet, inches);
+        System.out.println(feet + " feet and " + inches + " inches = " + centimeters + " cm");
+    }
 }
