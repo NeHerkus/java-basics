b98fd2b4748b8dcc06eb9955f2b44407c0a8542a
Darius <mlecka.darius@gmail.com>
2024-03-05 15:18:34 +0200

    task3

--- src/lt/itacademy/java/basics/Arithmetic.java
+++ src/lt/itacademy/java/basics/Arithmetic.java
@@ -1,5 +1,6 @@
 package lt.itacademy.java.basics;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Arithmetic {
@@ -19,5 +20,39 @@ public class Arithmetic {
 
         System.out.println("Cube’s volume: "+len*len*len +"cm3");
         System.out.println("\t Cube’s perimeter: "+len*12+"cm");
+
+        //Task 4
+        System.out.println("TASK 4\n");
+
+//        Create a method which converts feet and inches to centimeters. It needs to have two parameters.
+//        First parameter is feet and second is inches.
+//
+//        Hint: 1 inch = 2.54cm and 1 foot = 12 inches
+//
+//        Input: 2.4 4
+//
+//        Output: 2.4 feet and 4.0 inches = 83.312 cm
+
+        System.out.print("Input feets: ");
+        double feet = Double.parseDouble(scan.nextLine());
+
+        System.out.print("Input inches: ");
+        double inches = Double.parseDouble(scan.nextLine());
+
+        converterToCentimeters(feet,inches);
+
+    }
+    static void converterToCentimeters (double feet, double inches){
+double feetToCm = feet*12*2.54;
+double inchesToCm = inches*2.54;
+double sum = feetToCm+inchesToCm;
+        DecimalFormat df = new DecimalFormat("#.###");
+        sum = Double.valueOf(df.format(sum));
+
+        System.out.println("Output: "+feet+" feet and "+inches+" inches = "+sum+" cm");
+
+
+
+
     }
 }
