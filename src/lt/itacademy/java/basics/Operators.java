f38b0df72366f2f2dbf68b25061d1943945ca8db
Andrius Ledas <ledas.andrius@gmail.com>
2024-03-05 15:57:26 +0200

    1st task commit.

---
+++ src/lt/itacademy/java/basics/Operators.java
@@ -0,0 +1,24 @@
+void main() {
+
+    //System.out.println(determineTriangleType(5, 2, 3));
+
+}
+
+String determineTriangleType(int sideA, int sideB, int sideC) {
+
+    // this is a dumb way to do this...
+
+    if (! (sideA <= sideB + sideC && sideB <= sideA + sideC && sideC <= sideA + sideB)) {
+        return String.format("Triangle is invalid. Sides: %d %d %d", sideA, sideB, sideC);
+    } else if (sideA == sideB && sideB == sideC) {
+        return String.format("Triangle is equilateral. Sides: %d %d %d", sideA, sideB, sideC);
+    } else if ( (sideA == sideB) || (sideB == sideC) ) {
+        return String.format("Triangle is isosceles. Sides: %d %d %d", sideA, sideB, sideC);
+    } else {
+        return String.format("Triangle is scalene. Sides: %d %d %d", sideA, sideB, sideC);
+    }
+
+
+
+
+}
