6101abf7a177b11013322fb1d357419b3e467cca
Gabija Sabeckyte <gabeckyte@gmail.com>
2024-03-05 13:55:44 +0200

    first change

---
+++ src/lt/itacademy/java/basics/Basics.java
@@ -0,0 +1,13 @@
+package lt.itacademy.java.basics;
+
+public class Basics {
+    public static void main(String[] args) {
+
+        String name = "Gabija";
+        String surname = "Sabeckyte";
+        int age = 23;
+        System.out.println("Hello my name is " + name + surname + " and I'm " + age + " years old. I am attending IT & Data\n" +
+                "Academy 2024!");
+    }
+
+}
