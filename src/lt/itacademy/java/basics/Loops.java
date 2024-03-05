90742e1bf6e4ad065f6baaa8eee4e935d3af0e9e
Darius <mlecka.darius@gmail.com>
2024-03-05 19:23:53 +0200

    task9

--- src/lt/itacademy/java/basics/Loops.java
+++ src/lt/itacademy/java/basics/Loops.java
@@ -9,6 +9,10 @@ public class Loops {
         int days = Integer.parseInt(scanner.nextLine());
 
         calculateMinutesAndSeconds(days);
+        System.out.println("Enter number: ");
+
+        int number = Integer.parseInt(scanner.nextLine());
+        calculateFactorial(number);
     }
 //TASK8
     static void calculateMinutesAndSeconds(int year) {
@@ -24,4 +28,13 @@ public class Loops {
         } else System.out.println("Enter valid number");
 
     }
+    //TASK9
+    static void calculateFactorial (int number){
+        int sum=1;
+        for (int i = 1; i <= number; i++) {
+            sum*=i;
+        }
+        System.out.println("Factorial of number "+number+" is "+sum+".");
+    }
+
 }
