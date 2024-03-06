01c6f284e611695bd43061b9c5830c5f81080d1b
Jamtit <majauskas.titas@gmail.com>
2024-03-06 02:45:45 +0200

    Task 11 - not complete

---
+++ src/lt/itacademy/java/basics/Art.java
@@ -0,0 +1,24 @@
+package lt.itacademy.java.basics;
+
+import java.util.Scanner;
+
+public class Art {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char treeChar = '#';
+
+        System.out.println("Input value: ");
+        int depthOfTree = scanner.nextInt();
+
+        for(int i = 1; i <= depthOfTree; i++){
+
+            for(int k = depthOfTree - (i - 1); k > 0; k--){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i + (i - 1); j++){
+                System.out.print(treeChar);
+            }
+            System.out.println(" ");
+        }
+    }
+}
