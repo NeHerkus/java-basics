6d290655fe3cfbaca11c6630160bee28be0812f5
Martynas Piliutis <martynas.piliutis@gmail.com>
2024-03-08 20:33:17 +0200

    MPiliutis Task7 completed.

--- src/lt/itacademy/java/basics/Loops.java
+++ src/lt/itacademy/java/basics/Loops.java
@@ -1,5 +1,29 @@
 package lt.itacademy.java.basics;
 
-public class Loops {
+import java.util.Scanner;
 
+import static java.lang.System.*;
+
+public class Loops {
+    public static void main(String[] args) {
+        hoursMinutesInDays();
+    }
+
+    public static void hoursMinutesInDays() {
+        Scanner scanner = new Scanner(in);
+        out.print("Enter days: ");
+        int d = Integer.parseInt(scanner.nextLine());
+        int h = 0;
+        int m = 0;
+
+        for (int i = 0; i < d; i++) {
+            h += 24;
+        }
+
+        for (int i = 0; i < h; i++) {
+            m += 60;
+        }
+
+        out.println("There are " + h + " hours or " + m + " minutes in " + d + "days.");
+    }
 }
